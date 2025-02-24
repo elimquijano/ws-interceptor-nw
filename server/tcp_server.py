fc84116ed63cbc1c364ea24ce5ddb5cb219bc638
Elim Quijano <elim1905quijano@gmail.com>
2025-02-24 10:23:23 -0500

    parsear coban2

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -199,9 +199,7 @@ def parse_gps103_data(raw_data):
 
     # Determinar el tipo de evento
     command = parts[1]
-    if command == '001':
-        data['type'] = 'location information'
-    elif command == 'help me':
+    if command == 'help me':
         data['type'] = 'SOS alarm'
     elif command == 'low battery':
         data['type'] = 'low battery alarm'
@@ -225,8 +223,8 @@ def parse_gps103_data(raw_data):
         data['type'] = 'vehicle fault notification'
     elif command == 'service':
         data['type'] = 'vehicle maintenance notification'
-    elif command == '100':
-        data['type'] = 'single track'
+    elif command == '001':
+        data['type'] = 'location information'
     elif command == '101':
         data['type'] = 'track upon time interval'
     elif command == '103':
@@ -288,7 +286,7 @@ def parse_gps103_data(raw_data):
     elif command == '161':
         data['type'] = 'server request photo retransmission'
     elif command == '170':
-        data['type'] = 'send LCD/Handset, dispatch screen (notice) data'
+        data['type'] = 'send LCD/Handset, dispatch screen (notice)'
     elif command == '171':
         data['type'] = 'phone call dispatch: center sends answer race request'
     elif command == '172':
@@ -310,7 +308,7 @@ def parse_gps103_data(raw_data):
     elif command == 'rfid':
         data['type'] = 'RFID'
     elif command == 'TPMS':
-        data['type'] = 'tyres pressure monitoring'
+        data['type'] = 'tyre pressure monitoring'
     else:
         data['type'] = 'unknown'
 
@@ -318,7 +316,7 @@ def parse_gps103_data(raw_data):
     if data['type'] in ['location information', 'SOS alarm', 'low battery alarm', 'movement alarm', 'over speed alarm', 'geo-fence alarm', 'ACC alarm', 'accident alarm', 'shock sensor alarm', 'door alarm', 'oil leak/oil theft alarm', 'vehicle fault notification', 'vehicle maintenance notification']:
         data['data'] = {
             'imei': parts[0],
-            'time': datetime.strptime(parts[2], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[2], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[3],
             'latitude': float(parts[4][:2]) + float(parts[4][2:]) / 60,
             'latitude_direction': parts[5],
@@ -332,11 +330,11 @@ def parse_gps103_data(raw_data):
             'oil_percentage_2': parts[13],
             'temperature': parts[14]
         }
-    elif data['type'] in ['single track', 'track upon time interval', 'track upon distance interval', 'cancel auto track continuously', 'cancel alarm', 'set movement alarm', 'cancel movement alarm', 'set overspeed alarm', 'set time zone', 'cut off oil and power', 'resume oil and power', 'arm', 'disarm', 'switch to SMS mode', 'set geo-fence', 'cancel geo-fence', 'data load', 'cancel upload', 'activate less GPRS mode', 'deactivate less GPRS mode', 'automatic update positions of vehicle turns', 'set multi-area management', 'set IP, port for address function', 'activate speed limit mode', 'deactivate speed limit mode', 'activate speed limit']:
+    elif data['type'] in ['track upon time interval', 'track upon distance interval', 'cancel auto track continuously', 'cancel alarm', 'set movement alarm', 'cancel movement alarm', 'set overspeed alarm', 'set time zone', 'cut off oil and power', 'resume oil and power', 'arm', 'disarm', 'switch to SMS mode', 'set geo-fence', 'cancel geo-fence', 'data load', 'cancel upload', 'activate less GPRS mode', 'deactivate less GPRS mode', 'automatic update positions of vehicle turns', 'set multi-area management', 'set IP, port for address function', 'activate speed limit mode', 'deactivate speed limit mode', 'activate speed limit']:
         data['data'] = {
             'imei': parts[0],
             'command': parts[1],
-            'time': datetime.strptime(parts[2], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[2], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[3],
             'latitude': float(parts[4][:2]) + float(parts[4][2:]) / 60,
             'latitude_direction': parts[5],
@@ -354,7 +352,7 @@ def parse_gps103_data(raw_data):
         data['data'] = {
             'imei': parts[0],
             'command': parts[1],
-            'time': datetime.strptime(parts[2], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[2], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[3],
             'latitude': float(parts[4][:2]) + float(parts[4][2:]) / 60,
             'latitude_direction': parts[5],
@@ -373,7 +371,7 @@ def parse_gps103_data(raw_data):
             'imei': parts[0],
             'command': parts[1],
             'photo_data_amount': parts[2],
-            'time': datetime.strptime(parts[3], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[3], '%H%M%S').strftime('%H:%M:%S'),
             'phone_number': parts[4],
             'gps_valid': parts[5],
             'latitude': float(parts[6][:2]) + float(parts[6][2:]) / 60,
@@ -388,12 +386,12 @@ def parse_gps103_data(raw_data):
             'oil_percentage_2': parts[15],
             'temperature': parts[16]
         }
-    elif data['type'] in ['send LCD/Handset, dispatch screen (notice) data', 'phone call dispatch: center sends answer race request', 'phone call dispatch: center sends answer successfully', 'phone call dispatch: center sends answer failed', 'phone call dispatch: center cancels order', 'driver hands in answer order', 'driver cancels order', 'driver finishes task']:
+    elif data['type'] in ['send LCD/Handset, dispatch screen (notice)', 'phone call dispatch: center sends answer race request', 'phone call dispatch: center sends answer successfully', 'phone call dispatch: center sends answer failed', 'phone call dispatch: center cancels order', 'driver hands in answer order', 'driver cancels order', 'driver finishes task']:
         data['data'] = {
             'imei': parts[0],
             'command': parts[1],
             'order_no': parts[2],
-            'time': datetime.strptime(parts[3], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[3], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[4],
             'latitude': float(parts[5][:2]) + float(parts[5][2:]) / 60,
             'latitude_direction': parts[6],
@@ -412,7 +410,7 @@ def parse_gps103_data(raw_data):
             'imei': parts[0],
             'command': parts[1],
             'ad_code': parts[2],
-            'time': datetime.strptime(parts[3], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[3], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[4],
             'latitude': float(parts[5][:2]) + float(parts[5][2:]) / 60,
             'latitude_direction': parts[6],
@@ -431,7 +429,7 @@ def parse_gps103_data(raw_data):
             'imei': parts[0],
             'command': parts[1],
             'rfid_tag': parts[2],
-            'time': datetime.strptime(parts[3], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[3], '%H%M%S').strftime('%H:%M:%S'),
             'gps_valid': parts[4],
             'latitude': float(parts[5][:2]) + float(parts[5][2:]) / 60,
             'latitude_direction': parts[6],
@@ -445,11 +443,11 @@ def parse_gps103_data(raw_data):
             'oil_percentage_2': parts[14],
             'temperature': parts[15]
         }
-    elif data['type'] == 'tyres pressure monitoring':
+    elif data['type'] == 'tyre pressure monitoring':
         data['data'] = {
             'imei': parts[0],
             'command': parts[1],
-            'time': datetime.strptime(parts[2], '%y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
+            'time': datetime.strptime(parts[2], '%H%M%S').strftime('%H:%M:%S'),
             'tyre_status': parts[3],
             'left_front_tyre_pressure': parts[4],
             'left_front_tyre_temperature': parts[5],
