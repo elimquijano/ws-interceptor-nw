3713bdc37480676d033d1ef9486b095e5c52fd8a
Elim Quijano <elim1905quijano@gmail.com>
2025-02-24 15:37:06 -0500

    problema en handle5

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,12 +1,13 @@
 import asyncio
 import websockets
+from urllib.parse import urlparse
 from .utils import authenticate_client, clients
 
-
-async def handle_client(websocket):  # Remove path parameter
+async def handle_client(websocket):
     print("handle_client is being executed!")
-    # Get the path from websocket object
+    # Get the path and query string from the websocket connection
     path = websocket.path
+    
     devices = await authenticate_client(websocket, path)
     if not devices:
         return
@@ -23,12 +24,10 @@ async def handle_client(websocket):  # Remove path parameter
     finally:
         del clients[client_id]
 
-
 async def main():
     server = await websockets.serve(handle_client, "0.0.0.0", 7006)
     print("WebSocket server listening on port 7006")
     await server.wait_closed()
-
 
 def start_websocket_server():
     loop = asyncio.new_event_loop()
