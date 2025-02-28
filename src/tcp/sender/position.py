a7a6b23b4e736878145904d0cf0fa5f59732322e
Elim Quijano <elim1905quijano@gmail.com>
2025-02-28 12:02:29 -0500

    probar validador de datetime sinotrack

--- src/tcp/sender/position.py
+++ src/tcp/sender/position.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-def update_position(event, devices):
-    if event["type"] == "position" and chek_datetime_valid(event["data"]["datetime"]):
+def update_position(port, event, devices):
+    if event["type"] == "position" and check_datetime_valid(port, event["data"]["datetime"]):
         data = event["data"]
         for device in devices:
             if device["uniqueid"] == data["imei"]:
@@ -12,7 +12,26 @@ def update_position(event, devices):
                 break
     return devices
 
-def chek_datetime_valid(datetime_str):
-    print(f"VEHICULO: {datetime_str}")
-    print(f"ACTUAL: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    return True
+def check_datetime_valid(port, datetime_str):
+    if port == 6013:  # Sinotrack
+        # Convertir la cadena de fecha y hora del vehículo a un objeto datetime
+        vehicle_datetime = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+        # Obtener la hora actual
+        current_datetime = datetime.now()
+
+        # Calcular el rango aceptable (una hora menos y un máximo de 5 segundos de diferencia)
+        min_acceptable_datetime = current_datetime - timedelta(hours=1, seconds=5)
+        max_acceptable_datetime = current_datetime + timedelta(seconds=5)
+
+        # Imprimir los valores para depuración
+        print(f"VEHICULO: {vehicle_datetime}")
+        print(f"ACTUAL: {current_datetime}")
+        print(f"Rango aceptable: {min_acceptable_datetime} - {max_acceptable_datetime}")
+
+        # Verificar si la fecha y hora del vehículo está dentro del rango aceptable
+        if min_acceptable_datetime <= vehicle_datetime <= max_acceptable_datetime:
+            return True
+        else:
+            return False
+    else:
+        return False
